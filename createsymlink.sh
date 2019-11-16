7a9040e4e9014e8d4ba6b44146db574150d2dca6
Michael Andersson <m-andersson@outlook.com>
2019-11-16 22:03:36 +0100

    Update createsymlink.sh

--- createsymlink.sh
+++ createsymlink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s /home/$USER/gitstuff/linux-dotConfigFiles/bashrc /home/micand/.bashrc
-ln -s /home/$USER/gitstuff/linux-dotConfigFiles/config /home/micand/.i3/config
-ln -s /home/$USER/gitstuff/linux-dotConfigFiles/inputrc /home/micand/.inputrc
-ln -s /home/$USER/gitstuff/linux-dotConfigFiles/vimrc  /home/micand/.vimrc
+ln -s /home/$USER/gitstuff/linux-dotConfigFiles/bashrc /home/$USER/.bashrc
+ln -s /home/$USER/gitstuff/linux-dotConfigFiles/config /home/$USER/.i3/config
+ln -s /home/$USER/gitstuff/linux-dotConfigFiles/inputrc /home/$USER/.inputrc
+ln -s /home/$USER/gitstuff/linux-dotConfigFiles/vimrc  /home/$USER/.vimrc
